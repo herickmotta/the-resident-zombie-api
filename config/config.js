c302d6903cee0eeb75e4a7526daaecde913fcdf4
herickmotta <herickrossato@gmail.com>
2021-02-24 13:48:36 -0300

    db migrations fix dialect

--- config/config.js
+++ config/config.js
@@ -5,8 +5,26 @@ dotenv.config();
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.DATABASE_URL,
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
